d564bdbdc9314eb2e92093e6e6f03cf4d79fef37
Chase Gwuap <ambaza02@gmail.com>
2026-01-05 20:28:38 -0500

    Add about page and update routing

--- main.go
+++ main.go
@@ -7,6 +7,74 @@ import (
 )
 
 func main() {
+	// Create a new ServeMux
+	mux := http.NewServeMux()
+
+	// Static files
+	mux.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
+	// Load templates
+	tmpl := template.Must(template.ParseGlob("templates/*.html"))
+
+	// Home route
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "index.html", nil)
+	})
+
+	// About route
+	mux.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "about.html", nil)
+	})
+
+	log.Println("Server running 🚀 on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+/*package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Static files
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
+	// Load all templates
+	tmpl := template.Must(template.ParseGlob("templates/*.html"))
+
+	// Home
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "index.html", nil)
+	})
+
+	// About
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "about.html", nil)
+	})
+
+	log.Println("Server running 🚀 on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
+
+
+/*package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
 	// Serve static files (CSS)
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
@@ -20,6 +88,14 @@ func main() {
 		tmpl.Execute(w, nil)
 	})
 
+	// About route
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("About Page"))
+	})
+
 	log.Println("Server running🚀🚀 on http://localhost:8080")
+	// log.Println("Server running🚀🚀 on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+``
+*/
